9123f782dd7d1c9847672b7cd62c2cfd2e728cdf
Mark Hobson <mark.hobson@blackpepper.co.uk>
2020-01-02 20:43:14 +0000

    Load contacts from CSV

--- book.rb
+++ book.rb
@@ -18,13 +18,29 @@ module Contacts
       end
     end
 
+    def load(filename)
+      File.open(filename, "r") do |file|
+        file.gets
+        file.each_line do |line|
+          (name, mobile) = line.split(',').collect do |field|
+            field.strip
+          end
+          add(Contact.new(name, mobile))
+        end
+      end
+    end
+
     def save(filename)
       File.open(filename, "w") do |file|
         file.puts "Name, Mobile"
         each do |contact|
-          file.puts "#{contact.name}, #{contact.mobile}"
+          file.puts [contact.name, contact.mobile].join(", ")
         end
       end
+    end
+
+    def to_s
+      @contacts.to_s
     end
   end
 end
--- main.rb
+++ main.rb
@@ -1,7 +1,5 @@
 require './book'
 
 book = Contacts::Book.new
-book.add(Contacts::Contact.new("Alice", "07900 111 111"))
-book.add(Contacts::Contact.new("Bob", "07900 222 222"))
-book.add(Contacts::Contact.new("Carol", "07900 333 333"))
-book.save("book.csv")
+book.load("book.csv")
+puts book
